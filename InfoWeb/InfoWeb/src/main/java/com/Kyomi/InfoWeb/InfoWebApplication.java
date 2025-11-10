bbb199d524a0598f7f6675346f1c739fd7c6a10b
Kyomitaka <kiyomi.takahashi.cr@gmail.com>
2025-11-09 21:28:56 -0600

    Cambios para poder realizar la conexion con la BD

---
+++ InfoWeb/InfoWeb/src/main/java/com/Kyomi/InfoWeb/InfoWebApplication.java
@@ -0,0 +1,13 @@
+package com.Kyomi.InfoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoWebApplication.class, args);
+	}
+
+}
---
+++ InfoWeb/InfoWeb/src/test/java/com/Kyomi/InfoWeb/InfoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kyomi.InfoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
